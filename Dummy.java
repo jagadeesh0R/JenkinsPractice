100b6006fbf2ba78cc70c3ffd1d38c2addbf44f9
Jagadeesh59+43 <113662573+jagadeesh0R@users.noreply.github.com>
2023-02-21 14:28:43 +0530

    Update Dummy.java

--- Dummy.java
+++ Dummy.java
@@ -3,6 +3,7 @@ public class Dummy{
 public static void main(String[]args){
 
 System.out.print("Triggered by git push with jenkins");
+  System.out.print("ngrok is used as external service to link git and localhost");
 
 }
 
