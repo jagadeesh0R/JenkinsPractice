09fa1479b0988b85bb1c6a9b2e7d21704e046f69
Jagadeesh59+43 <113662573+jagadeesh0R@users.noreply.github.com>
2023-02-21 14:47:23 +0530

    Update Dummy.java

--- Dummy.java
+++ Dummy.java
@@ -4,6 +4,8 @@ public static void main(String[]args){
 
 System.out.print("Triggered by git push with jenkins");
   System.out.print("ngrok is used as external service to link git and localhost");
+  System.out.print("After branch error i changed master to main as branch in jenkins");
+  
 
 }
 
